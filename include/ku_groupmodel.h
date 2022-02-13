f663ace626afe6308614a092c0a093e47f74c1b8
juliagoda <juliagoda.pl@protonmail.com>
2022-02-13 15:58:19 +0100

    fix: restore removed? in not understandable way files

---
+++ include/ku_groupmodel.h
@@ -0,0 +1,56 @@
+/*
+ *  Copyright (c) 2006 Szombathelyi György <gyurco@freemail.hu>
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301, USA.
+ **/
+
+#ifndef _KU_GROUPMODEL_H_
+#define _KU_GROUPMODEL_H_
+
+#include <QAbstractTableModel>
+#include <QSortFilterProxyModel>
+
+#include "ku_group.h"
+
+class KU_GroupModel : public QAbstractTableModel
+{
+public:
+  virtual int rowCount( const QModelIndex & parent = QModelIndex() ) const override;
+  virtual int columnCount( const QModelIndex & parent = QModelIndex() ) const override;
+  virtual QVariant headerData( int section, Qt::Orientation orientation, int role = Qt::DisplayRole ) const override;
+  virtual QVariant data( const QModelIndex & index, int role = Qt::DisplayRole ) const override;
+  virtual bool insertRows( int row, int count, const QModelIndex & parent = QModelIndex() ) override;
+  virtual bool removeRows( int row, int count, const QModelIndex & parent = QModelIndex() ) override;
+  virtual bool setData( const QModelIndex & index, const QVariant & value, int role = Qt::EditRole ) override;
+
+  void init() { beginResetModel(); endResetModel(); }
+  void commitMod();
+  void commitDel();
+  void commitAdd();
+};
+
+class KU_GroupSortingProxyModel : public QSortFilterProxyModel
+{
+public:
+  void setFirstGroup( uint first ) { mFirstGroup = first; }
+protected:
+  virtual bool lessThan( const QModelIndex & left, const QModelIndex & right ) const override;
+  virtual bool filterAcceptsRow( int source_row, const QModelIndex & source_parent ) const override;
+private:
+  uint mFirstGroup;
+};
+
+#endif // _KU_GROUPMODEL_H_
---
+++ src/ku_deluser.cpp
@@ -0,0 +1,53 @@
+/*
+ *  Copyright (c) 2002 Waldo Bastian <bastian@kde.org>
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public
+ *  License version 2 or at your option version 3 as published by
+ *  the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301, USA.
+ **/
+
+#include "ku_deluser.h"
+
+#include "globals.h"
+
+#include <QLabel>
+#include <QFile>
+#include <QDialogButtonBox>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <KI18n/KLocalizedString>
+
+#include <QLocale>
+
+KU_DelUser::KU_DelUser(KU_User *AUser, QWidget *parent)
+ : QDialog( parent)
+{
+  setWindowTitle(i18n("Delete User"));
+  QDialogButtonBox * buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
+  QPushButton* ok_button = buttonBox->button(QDialogButtonBox::Ok);
+  ok_button->setAutoDefault(true);
+
+  QVBoxLayout *page = new QVBoxLayout(this);
+  QLabel* label = new QLabel( i18n("<p>Deleting user <b>%1</b>"
+                   "<br />Also perform the following actions:</p>", AUser->getName()));
+  page->addWidget(label);
+  m_deleteHomeDir = new QCheckBox(i18n("Delete &home folder: %1", AUser->getHomeDir()));
+  page->addWidget(m_deleteHomeDir);
+  QString mailboxpath = QFile::decodeName(MAIL_SPOOL_DIR) + QLatin1Char( '/' ) + AUser->getName();
+  m_deleteMailBox = new QCheckBox(i18n("Delete &mailbox: %1", mailboxpath));
+  page->addWidget(m_deleteHomeDir);
+  page->addWidget(buttonBox);
+}
+
+
